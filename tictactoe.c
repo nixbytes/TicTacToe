89dc645c63f6ad22a46fc6f85a5e5faf24265563
nixbytes <real8686@gmail.com>
2023-10-24 21:41:42 -0400

    add empty functions

--- tictactoe.c
+++ tictactoe.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
 
+void resetBoard();
+void printBoard();
+int checkFreeSpace();
+void playerMove();
+void compuerMove();
+char checkWinner();
+void printWinner(char);
 
 int main(){
 
     puts("Hello C");
 
     return 0;
+}
+
+
+void resetBoard(){
+}
+void printBoard(){
+}
+int checkFreeSpace(){
+}
+void playerMove(){
+}
+void compuerMove(){
+}
+char checkWinner(){
+}
+void printWinner(char winner){
 }
