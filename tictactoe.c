a135f4b92901babe9e84582cc2d3a299fa7db238
nixbytes <real8686@gmail.com>
2023-10-27 23:35:25 -0400

    remove extra )

--- tictactoe.c
+++ tictactoe.c
@@ -35,11 +35,11 @@ void resetBoard() {
   }
 }
 void printBoard() {
-    printf(" %c | %c | %c ",board[0][0],board[0][1],board[0][2])));
+    printf(" %c | %c | %c ",board[0][0],board[0][1],board[0][2]);
     printf("\n---|---|---\n");"
-    printf(" %c | %c | %c ",board[1][0],board[1][1],board[1][2])));
+    printf(" %c | %c | %c ",board[1][0],board[1][1],board[1][2]);
     printf("\n---|---|---\n");"
-    printf(" %c | %c | %c ",board[2][0],board[2][1],board[2][2])));
+    printf(" %c | %c | %c ",board[2][0],board[2][1],board[2][2]);
     printf("\n---|---|---\n");
     "
 }
